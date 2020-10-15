c18f390defdf39cefe2d38a6b0c75c156f0ad295
Robert Pirritano <rpirritano@gmail.com>
2020-10-15 15:56:02 -0700

    initial setup with template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {EcommerceService} from "./ecommerce/services/ecommerce.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    providers: [EcommerceService]
 })
 export class AppComponent {
-  title = 'AngOct15';
+
 }
